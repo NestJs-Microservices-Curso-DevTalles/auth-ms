d05fbe0fa84af19bbf7f33dd21a9ad4498e8b211
Jorge Octavio <jorgeoctavio35@gmail.com>
2024-06-20 17:49:47 -0600

    add register method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,57 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto';
+import { RpcException } from '@nestjs/microservices';
 @Injectable()
-export class AuthService {
+export class AuthService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger('AuthService');
+
+
+    onModuleInit() {
+        this.$connect();
+        this.logger.log('Mongo Database connected');
+    }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+        const { email, name, password } = registerUserDto;
+    
+        try {
+          const user = await this.user.findUnique({
+            where: {
+              email: email,
+            },
+          });
+    
+          if (user) {
+            throw new RpcException({
+              status: 400,
+              message: 'User already exists',
+            });
+          }
+    
+          const newUser = await this.user.create({
+            data: {
+              email: email,
+            //   password: bcrypt.hashSync(password, 10), // TODO: encriptar / hash
+            password: password,
+              name: name,
+            },
+          });
+    
+          const { password: __, ...rest } = newUser;
+    
+          return {
+            user: rest,
+            // token: await this.signJWT(rest),
+            token: 'token',
+          };
+        } catch (error) {
+          throw new RpcException({
+            status: 400,
+            message: error.message,
+          });
+        }
+      }
+
+
 }
