942d483800cccbc7f9610ac5b34c1f5b124982c4
Jorge Octavio <jorgeoctavio35@gmail.com>
2024-06-21 08:02:04 -0600

    add dockerfile prod

--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
 # FROM node:21-slim
 # FROM node:21-alpine3.19
-FROM node:20-alpine3.20
+FROM node:20-alpine3.19
 
 WORKDIR /usr/src/app
 
--- src/main.ts
+++ src/main.ts
@@ -15,12 +15,12 @@ async function bootstrap() {
     },
   );
 
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //   }),
-  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen();
 }
